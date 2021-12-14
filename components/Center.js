6f9f0045438735c2cc3399dca4fa71c22159f7ac
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-12-14 03:06:33 +0300

    Create Center.js

---
+++ components/Center.js
@@ -0,0 +1,82 @@
+import { ChevronDownIcon } from "@heroicons/react/outline";
+import { signOut, useSession } from "next-auth/react";
+import { shuffle } from "lodash";
+import { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
+import Songs from "./Songs";
+const colors = [
+  "from-indigo-500",
+  "from-blue-500",
+  "from-green-500",
+  "from-red-500",
+  "from-yellow-500",
+  "from-pink-500",
+  "from-purple-500",
+];
+
+function Center() {
+  const { data: session } = useSession();
+  const [color, setColor] = useState(null);
+  const playlistId = useRecoilValue(playlistIdState);
+  const [playlist, setPlaylist] = useRecoilState(playlistState);
+  const spotifyApi = useSpotify();
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop());
+  }, [playlistId]);
+
+  useEffect(() => {
+    spotifyApi
+      .getPlaylist(playlistId)
+      .then((data) => {
+        setPlaylist(data.body);
+      })
+      .catch((err) => {
+        console.log("Something went wrong", err);
+      });
+  }, [spotifyApi, playlistId]);
+
+  console.log(playlist);
+  return (
+    <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
+      <header className="absolute top-5 right-8">
+        <div
+          className="flex items-center bg-red-300 space-x-3 opacity-90 
+        hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" onClick={signOut}
+        >
+          <img
+            src={session?.user.image}
+            alt=""
+            className="rounded-full w-10 h-10"
+          ></img>
+          <h2>{session?.user.name}</h2>
+          <ChevronDownIcon className="w-5 h-5" />
+        </div>
+      </header>
+
+      <section
+        className={`flex items-end bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
+      >
+        <img
+          className="h-44 w-44 shadow-2xl"
+          src={playlist?.images?.[0]?.url}
+          alt=""
+        />
+        <div className="m-3">
+          <p>PLAYLIST</p>
+          <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">
+            {playlist?.name}
+          </h1>
+        </div>
+      </section>
+
+      <div className="text-white">
+        <Songs />
+      </div>
+    </div>
+  );
+}
+
+export default Center;
