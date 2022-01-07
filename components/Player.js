9951df0fbb76eda3b89cb96758a5462d9777caf2
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-01-07 13:04:47 +0300

    create player component

---
+++ components/Player.js
@@ -0,0 +1,126 @@
+import { SwitchHorizontalIcon, VolumeUpIcon } from "@heroicons/react/outline";
+import {
+  RewindIcon,
+  PlayIcon,
+  ReplyIcon,
+  FastForwardIcon,
+  PauseIcon,
+} from "@heroicons/react/solid";
+import { debounce } from "lodash";
+import { useSession } from "next-auth/react";
+import { useCallback, useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import useSongInfo from "../hooks/useSongInfo";
+import useSpotify from "../hooks/useSpotify";
+
+function Player() {
+  const { data: session, status } = useSession();
+  const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(50);
+
+  const songInfo = useSongInfo();
+
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+        console.log(data.body?.item);
+        setCurrentTrackId(data.body?.item?.id);
+
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+          setIsPlaying(data.body?.is_playing);
+        });
+      });
+    }
+  };
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body.is_playing) {
+        spotifyApi.pause();
+        setIsPlaying(false);
+      } else {
+        spotifyApi.play();
+        setIsPlaying(true);
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken && !currentTrackId) {
+      fetchCurrentSong();
+      setVolume(50);
+    }
+  }, [currentTrackId, spotifyApi, session]);
+
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume);
+    }
+  }, [volume]);
+
+  const debouncedAdjustVolume = useCallback(
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((er) => {});
+    }, 100),
+    []
+  );
+
+  return (
+    <div
+      className="h-24 bg-gradient-to-t from-black to-gray-900 text-white grid grid-cols-3 
+    text-xs md:text-base px-2 md:px-8"
+    >
+      {/*Left side */}
+      <div className="flex items-center space-x-4">
+        <img
+          className="hidden md:inline h-10 w-10"
+          src={songInfo?.album.images?.[0]?.url}
+          alt=""
+        />
+        <div>
+          <h3>{songInfo?.name}</h3>
+          <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
+      </div>
+
+      {/* Center */}
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button" />
+        <RewindIcon className="button" />
+        {isPlaying ? (
+          <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        ) : (
+          <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        )}
+
+        <FastForwardIcon className="button" />
+        <ReplyIcon className="button" />
+      </div>
+
+      {/* Right */}
+      <div className="flex items-center space-x-3 md:space-x-4  justify-end pr-5">
+        <VolumeUpIcon
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+          className="button"
+        />
+        <input
+          type="range"
+          className="w-14 md:w-28"
+          value={volume}
+          min={0}
+          max={100}
+          onChange={(e) => setVolume(Number(e.target.value))}
+        ></input>
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Player;
