3ba098fc6f1ac1b947f6a62d423d81beff1cef49
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-01-07 13:04:07 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { getSession } from "next-auth/react";
 import Head from "next/head";
 import Center from "../components/Center";
+import Player from "../components/Player";
 import Siderbar from "../components/Siderbar";
 
 export default function Home() {
@@ -15,7 +16,9 @@ export default function Home() {
         <Center />
       </main>
 
-      <div>{/* Player */}</div>
+      <div className="sticky bottom-0">
+        <Player />
+      </div>
     </div>
   );
 }
